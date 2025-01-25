7117045e7389b1cfce5d8187dbb30611f903bec3
Alexey Novikov <alexey@novikov.io>
2025-01-24 21:56:10 -0800

    rewrite iterator_chop

--- kata02-karate-chop/wednesday.cpp
+++ kata02-karate-chop/wednesday.cpp
@@ -4,14 +4,16 @@ static
 std::vector<int>::const_iterator iterator_chop(std::vector<int>::const_iterator left,
                                                std::vector<int>::const_iterator right,
                                                int target) {
-    if (left == right)
-        return std::vector<int>::const_iterator();
-    auto middle = left + std::distance(left, right) / 2;
-    if (*middle < target)
-        return iterator_chop(middle + 1, right, target);
-    if (*middle > target)
-        return iterator_chop(left, middle, target);
-    return middle;
+    while (left != right) {
+        auto middle = left + std::distance(left, right) / 2;
+        if (*middle == target)
+            return middle;
+        if (*middle < target)
+            left = middle + 1;
+        else
+            right = middle;
+    }
+    return std::vector<int>::const_iterator();
 }
 
 ssize_t wednesday(const std::vector<int>& array, int target) {
