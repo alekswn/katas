c5b4ae2ba49297bfa6896e79d4dd212a1ac1d102
Alexey Novikov <alexey@novikov.io>
2025-06-29 10:49:40 -0700

    Add boilerplate for naive-malloc

---
+++ kata-malloc/naive-malloc/naive-malloc.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+
+void* malloc(size_t size) {
+    return NULL;
+}
+
+void free(void* ptr) {
+    return;
+}
+
+void* calloc(size_t nmemb, size_t size) {
+    return NULL;
+}
+
+void* realloc(void* ptr, size_t size) {
+    return NULL;
+}
